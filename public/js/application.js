bfc74df037dcd2e5fd0e7366b0ef1de17e1bf8de
Richard Vogt <richvogtjr@gmail.com>
2013-08-08 16:48:11 -0500

    add readme / notes file to use as a guide and move text file to try to correct 400 error. didn't work.

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,15 @@
-$(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
-});
+
+
+function reqListener () {
+  console.log(this.responseText);
+};
+
+var oReq = new XMLHttpRequest();
+oReq.onload = reqListener;
+
+// The third argument here "true" sets the call to be asynchronous.
+// If omitted, it will still be asynch
+oReq.open("get", "yourFile.txt", true);
+
+oReq.send();
